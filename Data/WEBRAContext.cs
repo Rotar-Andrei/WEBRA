fa0185b748249668af372231669abc0472aba4cd
Rotar Andrei <rotarandrei0107@gmail.com>
2024-01-14 01:42:01 +0200

    Add project files.

---
+++ Data/WEBRAContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WEBRA.Models;
+
+namespace WEBRA.Data
+{
+    public class WEBRAContext : DbContext
+    {
+        public WEBRAContext (DbContextOptions<WEBRAContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WEBRA.Models.Book> Book { get; set; } = default!;
+        public DbSet<WEBRA.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WEBRA.Data;
+using WEBRA.Models;
+
+namespace WEBRA.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WEBRA.Data.WEBRAContext _context;
+
+        public IndexModel(WEBRA.Data.WEBRAContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
